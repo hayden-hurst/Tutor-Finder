7daee12ea124b7aaf359848e2ffcc7125dfbd13d
arcade1010 <enwurster@gmail.com>
2025-02-25 15:07:11 -0500

    changed nav/header tags, updated tutor-list-container div. Added subject and year to userSchema

--- models/users.js
+++ models/users.js
@@ -5,7 +5,14 @@ const Schema = mongoose.Schema;
 // User Schema
 
 // const userSchema
-
+const userSchema = new Schema ({
+    firstName: {type: String, required: [true, 'First Name is required!']},
+    lastName: {type: String, required: [true, 'Last Name is required!']},
+    email: {type: String, required: [true, 'Email is required!']},
+    password: {type: String, required: [true, 'Password is required!']},
+    subject: {type: String, required: [true, 'Subject is required!']},
+    year: {type: String, required: [true, 'Subject is required!']},
+});
 
 
 // Password Hashing
