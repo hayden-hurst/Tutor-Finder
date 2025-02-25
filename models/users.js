6e3e47d802ad44c8c1bc38b9b4f542ef12499829
arcade1010 <enwurster@gmail.com>
2025-02-25 15:17:55 -0500

    removed subject and year from user schema (reverting bc im not sure of change)

--- models/users.js
+++ models/users.js
@@ -9,9 +9,7 @@ const userSchema = new Schema ({
     firstName: {type: String, required: [true, 'First Name is required!']},
     lastName: {type: String, required: [true, 'Last Name is required!']},
     email: {type: String, required: [true, 'Email is required!']},
-    password: {type: String, required: [true, 'Password is required!']},
-    subject: {type: String, required: [true, 'Subject is required!']},
-    year: {type: String, required: [true, 'Subject is required!']},
+    password: {type: String, required: [true, 'Password is required!']}
 });
 
 
